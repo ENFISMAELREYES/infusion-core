fd33ec064f7899ab815aed23555fcb97bfbf48be
ENFISMAELREYES <enf.ismaelreyes@gmail.com>
2026-05-30 22:17:05 -0600

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { initializeFirestore, CACHE_SIZE_UNLIMITED } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBXz5TRpGHX7nbFjQYjGJi2l17YBpxtjFw",
@@ -8,14 +8,10 @@ const firebaseConfig = {
   projectId: "infusion-core",
   storageBucket: "infusion-core.firebasestorage.app",
   messagingSenderId: "372027565230",
-  appId: "1:372027565230:web:9c2055e465b9ed7a02b93f"
+  appId: "1:372027565230:web:9c2055e465b9ed7a02b93f",
+  databaseId: "default"
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
-  useFetchStreams: false,
-});
-
+export const db = getFirestore(app, "default");
 export const auth = getAuth(app);
