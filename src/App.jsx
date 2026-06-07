6cd3196e1d0fe4f93f373bcaca58d19d153a19a8
ENFISMAELREYES <enf.ismaelreyes@gmail.com>
2026-06-07 09:31:02 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Monitor from "./pages/Monitor";
 import NurseView from "./pages/NurseView";
 import NuevaSession from "./pages/NuevaSession";
 import Historial from "./pages/Historial";
+import Catalogo from "./pages/Catalogo";
 
 function PrivateRoute({ children, roles }) {
   const { user, profile, loading } = useAuth();
@@ -48,6 +49,7 @@ function AppRoutes() {
         <Route path="monitor" element={<PrivateRoute roles={["jefe","visualizador"]}><Monitor /></PrivateRoute>} />
         <Route path="autorizar" element={<PrivateRoute roles={["jefe"]}><Autorizar /></PrivateRoute>} />
         <Route path="historial" element={<PrivateRoute roles={["jefe","visualizador"]}><Historial /></PrivateRoute>} />
+        <Route path="catalogo" element={<PrivateRoute roles={["jefe"]}><Catalogo /></PrivateRoute>} />
         <Route path="pacientes" element={<PrivateRoute roles={["enfermera"]}><NurseView /></PrivateRoute>} />
         <Route path="registrar" element={<PrivateRoute roles={["enfermera"]}><NuevaSession /></PrivateRoute>} />
         <Route path="*" element={<Navigate to={profile?.role === "enfermera" ? "/pacientes" : "/"} replace />} />
