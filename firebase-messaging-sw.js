2fba7ce8a7894109b610c4d2ec5c9472f10c1353
ENFISMAELREYES <enf.ismaelreyes@gmail.com>
2026-06-28 14:11:09 -0600

    Update Firebase configuration in service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,9 +5,9 @@ firebase.initializeApp({
   apiKey: "AIzaSyBXz5TRpGHX7nbFjQYjGJi2l17YBpxtjFw",
   authDomain: "infusion-core.firebaseapp.com",
   projectId: "infusion-core",
-  storageBucket: "infusion-core.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  storageBucket: "infusion-core.firebasestorage.app",
+  messagingSenderId: "372027565230",
+  appId: "1:372027565230:web:9c2055e465b9ed7a02b93f"
 });
 
 const messaging = firebase.messaging();
